80c393de197642f0e41c687aac3d1859fd106b6c
Lorenz Graf <lorenz.graaf@gmail.com>
2019-04-05 15:37:44 +0200

    camera feed init
    + screenshot

--- classy/src/app/app.module.ts
+++ classy/src/app/app.module.ts
@@ -6,15 +6,13 @@ import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
-import {NgProgressModule} from '@ngx-progressbar/core';
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
     imports: [BrowserModule,
         IonicModule.forRoot(),
-        AppRoutingModule,
-        NgProgressModule],
+        AppRoutingModule],
     providers: [
         {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
     ],
--- classy/src/app/home/home.page.ts
+++ classy/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import * as Tesseract from 'tesseract.js';
+import {NgProgress} from '@ngx-progressbar/core';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
     selector: 'app-home',
@@ -6,7 +9,50 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-    ngOnInit(): void {
+    screenShotButtonDisabled: boolean = true;
 
+    constraints = {
+        video: true,
+        audio: false
+    };
+
+    @ViewChild('video')
+    video;
+
+    @ViewChild('image')
+    image;
+
+    @ViewChild('canvas')
+    canvas;
+
+    ngOnInit(): void {
+        console.log(this.video);
+        console.log(this.image);
+        console.log(this.canvas);
     }
+
+    public captureVideoButtonClick() {
+        navigator.mediaDevices.getUserMedia(this.constraints)
+            .then((stream) => {
+                this.handleSuccess(stream);
+            }).catch(this.handleError);
+    };
+
+    public screenShotButtonClick() {
+        this.canvas.nativeElement.width = this.video.nativeElement.videoWidth;
+        this.canvas.nativeElement.height = this.video.nativeElement.videoHeight;
+        this.canvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement, 0, 0);
+        // Other browsers will fall back to image/png
+        this.image.nativeElement.src = this.canvas.nativeElement.toDataURL('image/webp');
+    };
+
+    public handleSuccess(stream) {
+        this.screenShotButtonDisabled = false;
+        this.video.nativeElement.srcObject = stream;
+    }
+
+    public handleError(err) {
+        console.error(err);
+    }
+
 }
