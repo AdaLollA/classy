6009b53dc8740fc83eff4603de5c856e59bf96cc
Lorenz Graf <lorenz.graaf@gmail.com>
2019-04-08 11:22:58 +0200

    tongue size

--- classy/src/app/tongue/tongue.component.ts
+++ classy/src/app/tongue/tongue.component.ts
@@ -9,22 +9,33 @@ export class TongueComponent implements OnInit {
   @ViewChild('tongue')
   tongue;
 
-  private initPosition: number;
+  private initTop: number;
   private prevPosition: number;
 
   constructor() { }
 
   ngOnInit() {
-    this.initPosition = this.tongue.el.getBoundingClientRect().top;
+    this.initTop = this.tongue.el.getBoundingClientRect().top;
   }
 
-  public dragDrop() {
+  public dragEnd(event) {
+    // todo commented code in this function is for snapping to top / bottom
+    // event.source._dragRef.reset();
+
     if (this.prevPosition < this.tongue.el.getBoundingClientRect().top) {
       // move down
-      console.log('down')
-    } else {
+      console.log('down');
+      /*
+      event.source._activeTransform = {x: 0, y: 0};
+      event.source._passiveTransform = {x: 0, y: 0};
+       */
+    } else if (this.prevPosition > this.tongue.el.getBoundingClientRect().top) {
       // move up
       console.log('up')
+      /*
+      event.source._activeTransform = {x: 0, y: -568};
+      event.source._passiveTransform = {x: 0, y: -568};
+       */
     }
   }
 
