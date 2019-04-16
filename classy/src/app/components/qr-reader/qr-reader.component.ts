5779775b388991728c876e588ea704a5b003e58c
Lorenz Graf <lorenz.graaf@gmail.com>
2019-04-17 01:58:56 +0200

    added angular emitter and inputs

--- classy/src/app/components/qr-reader/qr-reader.component.ts
+++ classy/src/app/components/qr-reader/qr-reader.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import jsQR from 'jsqr';
 
 @Component({
@@ -7,23 +7,31 @@ import jsQR from 'jsqr';
     styleUrls: ['./qr-reader.component.scss'],
 })
 export class QrReaderComponent implements OnInit {
+    @Input('outline')
+    outline: string = '#FF3B58';
+
+    @Input('stopWhenRead')
+    stopWhenRead: boolean = false; // todo
+
+    @Input('refreshRate') // todo
+    refreshRate: number = 0;
+
+    @Output('readQr')
+    readQr: EventEmitter<string> = new EventEmitter();
+
+    @Output('scanTick') // todo
+    scanTick: EventEmitter<boolean> = new EventEmitter();
+
     private video;
     private canvasElement;
     private canvas;
-    private loadingMessage;
-    private outputContainer;
-    private outputMessage;
-    private outputData;
+    private readyToRead: boolean = true;
 
     ngOnInit() {
         // Init ui elements
         this.video = document.createElement('video');
         this.canvasElement = document.getElementById('canvas') as any;
         this.canvas = this.canvasElement.getContext('2d');
-        this.loadingMessage = document.getElementById('loadingMessage');
-        this.outputContainer = document.getElementById('output');
-        this.outputMessage = document.getElementById('outputMessage');
-        this.outputData = document.getElementById('outputData');
 
         // Use facingMode: environment to attemt to get the front camera on phones
         navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment'}}).then((stream) => {
@@ -46,11 +54,8 @@ export class QrReaderComponent implements OnInit {
     }
 
     private tick() {
-        this.loadingMessage.innerText = '⌛ Loading video...';
-        if (this.video.readyState === this.video.HAVE_ENOUGH_DATA) {
-            this.loadingMessage.hidden = true;
+        if ((this.video.readyState === this.video.HAVE_ENOUGH_DATA) && this.readyToRead) {
             this.canvasElement.hidden = false;
-            this.outputContainer.hidden = false;
 
             this.canvasElement.height = this.video.videoHeight;
             this.canvasElement.width = this.video.videoWidth;
@@ -60,16 +65,13 @@ export class QrReaderComponent implements OnInit {
                 inversionAttempts: 'dontInvert',
             });
             if (code) {
-                this.drawLine(code.location.topLeftCorner, code.location.topRightCorner, '#FF3B58');
-                this.drawLine(code.location.topRightCorner, code.location.bottomRightCorner, '#FF3B58');
-                this.drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, '#FF3B58');
-                this.drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, '#FF3B58');
-                this.outputMessage.hidden = true;
-                this.outputData.parentElement.hidden = false;
-                this.outputData.innerText = code.data;
+                this.drawLine(code.location.topLeftCorner, code.location.topRightCorner, this.outline);
+                this.drawLine(code.location.topRightCorner, code.location.bottomRightCorner, this.outline);
+                this.drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, this.outline);
+                this.drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, this.outline);
+                this.readQr.emit(code.data);
             } else {
-                this.outputMessage.hidden = false;
-                this.outputData.parentElement.hidden = true;
+                // no qr found at the moment
             }
         }
         requestAnimationFrame(() => {
@@ -77,61 +79,12 @@ export class QrReaderComponent implements OnInit {
         });
     }
 
-    /*
-      var video = document.createElement("video");
-      var canvasElement = document.getElementById("canvas");
-      var canvas = canvasElement.getContext("2d");
-      var loadingMessage = document.getElementById("loadingMessage");
-      var outputContainer = document.getElementById("output");
-      var outputMessage = document.getElementById("outputMessage");
-      var outputData = document.getElementById("outputData");
-
-      function drawLine(begin, end, color) {
-        canvas.beginPath();
-        canvas.moveTo(begin.x, begin.y);
-        canvas.lineTo(end.x, end.y);
-        canvas.lineWidth = 4;
-        canvas.strokeStyle = color;
-        canvas.stroke();
-      }
-
-      // Use facingMode: environment to attemt to get the front camera on phones
-      navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
-        video.srcObject = stream;
-        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
-        video.play();
-        requestAnimationFrame(tick);
-      });
-
-      function tick() {
-        loadingMessage.innerText = "⌛ Loading video..."
-        if (video.readyState === video.HAVE_ENOUGH_DATA) {
-          loadingMessage.hidden = true;
-          canvasElement.hidden = false;
-          outputContainer.hidden = false;
-
-          canvasElement.height = video.videoHeight;
-          canvasElement.width = video.videoWidth;
-          canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-          var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-          var code = jsQR(imageData.data, imageData.width, imageData.height, {
-            inversionAttempts: "dontInvert",
-          });
-          if (code) {
-            drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
-            drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
-            drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
-            drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
-            outputMessage.hidden = true;
-            outputData.parentElement.hidden = false;
-            outputData.innerText = code.data;
-          } else {
-            outputMessage.hidden = false;
-            outputData.parentElement.hidden = true;
-          }
+    private didRead() {
+        if (this.refreshRate > 0) {
+            this.readyToRead = false;
+            setTimeout(() => {
+                this.readyToRead = true;
+            }, this.refreshRate);
         }
-        requestAnimationFrame(tick);
-      }
-     */
-
+    }
 }
--- classy/src/app/pages/home/home.page.ts
+++ classy/src/app/pages/home/home.page.ts
@@ -1,8 +1,4 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
-import {timer} from 'rxjs';
-
-import QRReader from 'qrcode-reader';
 
 @Component({
     selector: 'app-home',
@@ -10,79 +6,11 @@ import QRReader from 'qrcode-reader';
     styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-    public screenShotButtonDisabled: boolean = true;
-    private qr = new QRReader();
-
-    constraints = {
-        video: true,
-        audio: false
-    };
-
-    @ViewChild('video')
-    video;
-
-    @ViewChild('image')
-    image;
-
-    @ViewChild('canvas')
-    canvas;
-
-    constructor() {
-    }
-
     ngOnInit(): void {
-        // this.captureVideo();
-        // this.observableTimer();
-        // this.initQrReader();
+
     }
 
-    private initQrReader() {
-        this.qr.callback = function (err, result) {
-            if (result) {
-                console.log(result);
-            } else {
-                console.error(err);
-            }
-        };
+    readQr(data: string) {
+        console.log(data, 'in HOME');
     }
-
-    private observableTimer() {
-        const source = timer(500, 1000);
-        const abc = source.subscribe(val => {
-            // console.log(val, '-');
-            this.screenShot();
-        });
-    }
-
-    public captureVideo() {
-        navigator.mediaDevices.getUserMedia(this.constraints)
-            .then((stream) => {
-                this.handleSuccess(stream);
-            }).catch((error) => {
-            this.handleError(error);
-        });
-    };
-
-    public screenShot() {
-        this.canvas.nativeElement.width = this.video.nativeElement.videoWidth;
-        this.canvas.nativeElement.height = this.video.nativeElement.videoHeight;
-        this.canvas.nativeElement.getContext('2d').drawImage(this.video.nativeElement, 0, 0);
-        this.image.nativeElement.src = this.canvas.nativeElement.toDataURL('image/png');
-        this.readQR(this.image.nativeElement.src);
-    };
-
-    public handleSuccess(stream) {
-        this.screenShotButtonDisabled = false;
-        this.video.nativeElement.srcObject = stream;
-    }
-
-    public handleError(error) {
-        console.error(error);
-    }
-
-    public readQR(image) {
-        // http://thecodebarbarian.com/creating-qr-codes-with-node-js.html
-        this.qr.decode(image);
-    }
-
 }
