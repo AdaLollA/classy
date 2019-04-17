15145a297d267e102d2c5e029e9544864e019cc0
Lorenz Graf <lorenz.graaf@gmail.com>
2019-04-17 02:15:54 +0200

    stop when found implemented

--- classy/src/app/components/qr-reader/qr-reader.component.ts
+++ classy/src/app/components/qr-reader/qr-reader.component.ts
@@ -13,19 +13,15 @@ export class QrReaderComponent implements OnInit {
     @Input('stopWhenRead')
     stopWhenRead: boolean = false; // todo
 
-    @Input('refreshRate') // todo
-    refreshRate: number = 0;
+    @Input('disabled')
+    disabled: boolean = false;
 
     @Output('readQr')
     readQr: EventEmitter<string> = new EventEmitter();
 
-    @Output('scanTick') // todo
-    scanTick: EventEmitter<boolean> = new EventEmitter();
-
     private video;
     private canvasElement;
     private canvas;
-    private readyToRead: boolean = true;
 
     ngOnInit() {
         // Init ui elements
@@ -37,7 +33,7 @@ export class QrReaderComponent implements OnInit {
         navigator.mediaDevices.getUserMedia({video: {facingMode: 'environment'}}).then((stream) => {
             this.video.srcObject = stream;
             this.video.setAttribute('playsinline', true); // required to tell iOS safari we don't want fullscreen
-            this.video.play();
+            this.video.play().then(/*do nothing*/);
             requestAnimationFrame(() => {
                 this.tick();
             });
@@ -54,37 +50,36 @@ export class QrReaderComponent implements OnInit {
     }
 
     private tick() {
-        if ((this.video.readyState === this.video.HAVE_ENOUGH_DATA) && this.readyToRead) {
+        let foundQr: boolean = false;
+        if (this.video.readyState === this.video.HAVE_ENOUGH_DATA) {
             this.canvasElement.hidden = false;
 
             this.canvasElement.height = this.video.videoHeight;
             this.canvasElement.width = this.video.videoWidth;
             this.canvas.drawImage(this.video, 0, 0, this.canvasElement.width, this.canvasElement.height);
             let imageData = this.canvas.getImageData(0, 0, this.canvasElement.width, this.canvasElement.height);
-            let code = jsQR(imageData.data, imageData.width, imageData.height, {
-                inversionAttempts: 'dontInvert',
-            });
-            if (code) {
-                this.drawLine(code.location.topLeftCorner, code.location.topRightCorner, this.outline);
-                this.drawLine(code.location.topRightCorner, code.location.bottomRightCorner, this.outline);
-                this.drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, this.outline);
-                this.drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, this.outline);
-                this.readQr.emit(code.data);
-            } else {
-                // no qr found at the moment
+
+            if (!this.disabled) {
+                let code = jsQR(imageData.data, imageData.width, imageData.height, {
+                    inversionAttempts: 'dontInvert',
+                });
+                if (code) {
+                    this.drawLine(code.location.topLeftCorner, code.location.topRightCorner, this.outline);
+                    this.drawLine(code.location.topRightCorner, code.location.bottomRightCorner, this.outline);
+                    this.drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, this.outline);
+                    this.drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, this.outline);
+                    this.readQr.emit(code.data);
+                    foundQr = true;
+                } else {
+                    // no qr found at the moment
+                }
             }
         }
-        requestAnimationFrame(() => {
-            this.tick();
-        });
-    }
 
-    private didRead() {
-        if (this.refreshRate > 0) {
-            this.readyToRead = false;
-            setTimeout(() => {
-                this.readyToRead = true;
-            }, this.refreshRate);
+        if (!(foundQr && this.stopWhenRead)) {
+            requestAnimationFrame(() => {
+                this.tick();
+            });
         }
     }
 }
