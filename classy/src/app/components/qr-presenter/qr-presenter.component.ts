9383237a07ae681bc8952f968255b5cd11611667
Linus Heimböck <linus.heimboeck@live.at>
2019-05-09 19:57:16 +0200

    Implemented QR-Code-Generation

--- classy/src/app/components/qr-presenter/qr-presenter.component.ts
+++ classy/src/app/components/qr-presenter/qr-presenter.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var JSQR: any;
+
 @Component({
   selector: 'app-qr-presenter',
   templateUrl: './qr-presenter.component.html',
@@ -9,6 +11,33 @@ export class QrPresenterComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.generateQR("asdfgh");
+  }
+
+
+  private generateQR(value: string): void {
+
+    var qr = new JSQR();
+
+    var code = new qr.Code();
+    code.encodeMode = code.ENCODE_MODE.UTF8_SIGNATURE;
+    code.version = code.DEFAULT;
+    code.errorCorrection = code.ERROR_CORRECTION.H;
+
+    var input = new qr.Input();
+    input.data = value;
+    input.dataType = input.DATA_TYPE.TEXT;
+
+    var matrix = new qr.Matrix(input, code);
+    matrix.scale = 10;
+
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute('width', matrix.pixelWidth);
+    canvas.setAttribute('height', matrix.pixelWidth);
+    canvas.getContext('2d').fillStyle = 'rgb(0,0,0)';
+    matrix.draw(canvas, 0, 0);
+    document.getElementById("qrElement").appendChild(canvas);
+  }
 
 }
