447bf686225630b3ed8bc50cf24bc6ba6ce777ce
Linus Heimböck <linus.heimboeck@live.at>
2019-05-09 23:11:26 +0200

    Made QR-Code resizable

--- classy/src/app/components/qr-presenter/qr-presenter.component.ts
+++ classy/src/app/components/qr-presenter/qr-presenter.component.ts
@@ -11,10 +11,16 @@ export class QrPresenterComponent implements OnInit {
 
   constructor() { }
 
+  private qrSize = 10;
+
+  private qrMatrix: any = null;
+
+  private qrContainer: HTMLElement;
+
   ngOnInit() {
+    this.qrContainer = document.getElementById("qrElement");
     this.generateQR("asdfgh");
   }
-
 
   private generateQR(value: string): void {
 
@@ -30,14 +36,25 @@ export class QrPresenterComponent implements OnInit {
     input.dataType = input.DATA_TYPE.TEXT;
 
     var matrix = new qr.Matrix(input, code);
-    matrix.scale = 10;
+    this.qrMatrix = matrix;
+    this.resizeQr(this.qrSize);
+  }
 
-    var canvas = document.createElement('canvas');
-    canvas.setAttribute('width', matrix.pixelWidth);
-    canvas.setAttribute('height', matrix.pixelWidth);
-    canvas.getContext('2d').fillStyle = 'rgb(0,0,0)';
-    matrix.draw(canvas, 0, 0);
-    document.getElementById("qrElement").appendChild(canvas);
+  private resizeQr(value: number) {
+    this.qrSize = value;
+    if(!!this.qrMatrix) {
+      this.qrMatrix.scale = this.qrSize;
+
+      var canvas = document.createElement('canvas');
+      canvas.setAttribute('width', this.qrMatrix.pixelWidth);
+      canvas.setAttribute('height', this.qrMatrix.pixelWidth);
+      canvas.getContext('2d').fillStyle = 'rgb(0,0,0)';
+      this.qrMatrix.draw(canvas, 0, 0);
+      while (this.qrContainer.hasChildNodes()) {
+        this.qrContainer.firstChild.remove();
+      }
+      this.qrContainer.appendChild(canvas);
+    }
   }
 
 }
