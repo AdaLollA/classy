dbef10d7a8527dcdb3f9fcdb76842d96e6e0ee82
Linus Heimböck <linus.heimboeck@live.at>
2019-06-12 18:18:24 +0200

    Added input and output properties

--- classy/src/app/components/login-card/login-card.component.ts
+++ classy/src/app/components/login-card/login-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input, SimpleChanges } from '@angular/core';
+import { IonProgressBar } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-card',
@@ -7,12 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginCardComponent implements OnInit {
 
+  @Input('loading') loading = false;
+
+  @Output('onImageLogin') imageLoginEmitter = new EventEmitter<boolean>();
+
+  public progressBarType = "determinate";
+
+  public buttonDisabled = false;
+
   constructor() { }
 
   ngOnInit() {}
 
+  ngOnChanges(changes: SimpleChanges) {
+    const loadingChange: boolean = changes.name.currentValue;
+    if (loadingChange == true) {
+      this.buttonDisabled = true;
+      this.progressBarType = "indeterminate";
+    } else {
+      this.buttonDisabled = false;
+      this.progressBarType = "determinate";
+    }
+    console.log("Loading: " + this.loading);
+  }
+
   loginClicked() {
     console.log("Login clicked")
+    this.imageLoginEmitter.emit(true);
   }
 
 }
