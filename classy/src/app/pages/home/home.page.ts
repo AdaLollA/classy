ff35f53f4ded54f8a2ac8880ecc0419e2ccb1c69
Lorenz Graf <lorenz.graaf@gmail.com>
2019-06-13 19:24:24 +0200

    tongue mode opaque on scan

--- classy/src/app/pages/home/home.page.ts
+++ classy/src/app/pages/home/home.page.ts
@@ -1,15 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {Platform} from '@ionic/angular';
 import {ThemeService} from '../../services/theme/theme.service';
 import { disableBindings } from '@angular/core/src/render3';
+import {TongueComponent} from '../../components/tongue/tongue.component';
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.page.html',
     styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, AfterViewInit {
     private disableScanning = false;
+
+   @ViewChild(TongueComponent)
+    public tongue: TongueComponent;
 
     constructor(public platform: Platform, private readonly theme: ThemeService) {
 
@@ -21,6 +25,7 @@ export class HomePage implements OnInit {
 
     readQr(data: string) {
         console.log(data, 'in HOME');
+        this.tongue.tongueModeOpaque(null);
     }
 
     tongueModeChange(inForeground: boolean) {
@@ -34,5 +39,10 @@ export class HomePage implements OnInit {
 
     public switchTheme(): void {
         this.theme.cycleTheme();
+    }
+
+    ngAfterViewInit(): void {
+        // this.tongue = document.getElementById("tongue");
+        console.log(this.tongue, 'tongue after init');
     }
 }
